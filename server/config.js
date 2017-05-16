441f3c507e8564216efec4b1ca0b27634e6b3771
jas0ncn <ceocjy@vip.qq.com>
2017-05-16 19:23:35 +0800

    add tips and remove infomations

--- server/config.js
+++ server/config.js
@@ -2,15 +2,32 @@ const CONF = {
     port: '9993',
     ROUTE_BASE_PATH: '/applet',
 
-    cosAppId: '1251771687',
+    /**
+     * COS 信息配置
+     * 查看：https://console.qcloud.com/capi
+     */
+
+    // APPID
+    cosAppId: '',
+    /**
+     * 区域
+     * 华北：cn-north
+     * 华东：cn-east
+     * 华南：cn-south
+     * 西南：cn-southwest
+     */
     cosRegion: 'cn-north',
-    cosSecretId: 'AKIDEMKMI7nMJ86wkPdEMJ56DXkMNBpXdUoL',
-    cosSecretKey: 'dat9EUaVGqIYouocIoHdputw1wrbAYbn',
-    cosFileBucket: 'photos',
+    // SecretId
+    cosSecretId: '',
+    // SecretKey
+    cosSecretKey: '',
+    // Bucket 名称
+    cosFileBucket: '',
+    // 文件夹
     cosUploadFolder: '/'
 }
 
-// 生成访问 COS 的域名
+// 生成访问 COS 的域名，无需修改
 CONF.cosDomain = (() => `http://${CONF.cosFileBucket}-${CONF.cosAppId}.costj.myqcloud.com/`)()
 
 module.exports = CONF
