dfed13633239cad87c15a1578837fef3c2a35a22
jasonelchen <jasonelchen@tencent.com>
2017-05-16 17:07:12 +0800

    update cos inti function

--- server/services/cos/index.js
+++ server/services/cos/index.js
@@ -1,6 +1,11 @@
-const { cos, auth, conf } = require('qcloud_cos');
-const config = require('../../config');
+const COS = require('cos-nodejs-sdk-v5')
+const _cosConfig = require('cos-nodejs-sdk-v5/sdk/config')
+const config = require('../../config')
 
-conf.setAppInfo(config.cosAppId, config.cosSecretId, config.cosSecretKey);
+/**
+ * init COS config
+ * see: https://github.com/tencentyun/cos-nodejs-sdk-v5/blob/master/sdk/config.js#L24
+ */
+_cosConfig.setAppInfo(config.cosAppId, config.cosSecretId, config.cosSecretKey);
 
-module.exports = Object.assign(cos, { auth, conf });
+module.exports = COS
